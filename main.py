b0e5d2b24f01244b6323649001db472b0411bdc7
Vladislav <v_kibirev@list.ru>
2023-04-18 20:58:34 +0500

    Removed exceptionhook

--- main.py
+++ main.py
@@ -90,10 +90,6 @@ class SecondForm(QWidget):
         self.label_35.setText(result[7])
 
 
-def exception_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = FirstForm()
